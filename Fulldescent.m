40444ed53d7a33dbc3adf9f2bfbd8684ecf2246d
joshyb-uon <62127100+joshyb-uon@users.noreply.github.com>
2020-08-15 15:37:24 +1000

    Playing with different values

--- Fulldescent.m
+++ Fulldescent.m
@@ -1,27 +1,27 @@
 %Setup paramaters
 %All units are SI (NO FEET!)
 samples = 1e6; %number of diecreet time steps
-duration = 100; %(s) duration of the simulation, needs a abtter way to find this time
+duration = 150; %(s) duration of the simulation, needs a abtter way to find this time
 timeInterval = duration/samples;
 last = samples;
 
-apogee = 1000; %(m) Divide by  3.281 for feet
+apogee = 1310; %(m) Divide by  3.281 for feet
 %Select either to have the chute deploy at either a certain height or
 %time after apogee, if using height, set time to duration+1, if using
 %time, set height to 0
-mainDeployTime = 30;
-mainDeployHeight = 0;
-drougeDeployTime = 10;
+mainDeployTime = duration+1;
+mainDeployHeight = 100;
+drougeDeployTime = 3;
 drougeDeployHeight = 0;
 
 %Drag data for the rocket, drouge and main chute
 %Data from OpenRocket
 rocketCd = 0.44;
 rocketA = 9.67e-3;
-mainCd = 0.75;
-mainA = 1.27^2/4*pi;
-drougeCd = 0.75;
-drougeA = 0.46^2/4*pi;
+mainCd = 0.97;
+mainA = 1.7;
+drougeCd = 1.5;
+drougeA = .289;
 mass = 3;
 
 %Enviromental data
